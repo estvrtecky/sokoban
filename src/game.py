858ff7663092ee7877565500dbdd956ca499aca1
StratenyNinja <erik.stvrtecky2000@gmail.com>
2024-04-08 11:10:25 +0200

    Add basic game structure

---
+++ src/game.py
@@ -0,0 +1,11 @@
+import pygame
+
+
+class Game:
+    def __init__(self):
+        # Pygame initialization
+        pygame.init()
+        pygame.font.init()
+
+    def run(self):
+        print("Game is running")
