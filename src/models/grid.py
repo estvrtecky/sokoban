f467f149f17750c1594e75e88b656304457a0771
StratenyNinja <erik.stvrtecky2000@gmail.com>
2024-04-08 16:56:06 +0200

    Refactor grid drawing logic to use a dictionary for color mapping

--- src/models/grid.py
+++ src/models/grid.py
@@ -3,6 +3,15 @@ import pygame
 from .worker import Worker
 
 
+# Constants
+BOX = "$"
+BOX_ON_STORAGE = "*"
+FLOOR = " "
+PLAYER = "@"
+PLAYER_ON_STORAGE = "+"
+STORAGE = "."
+WALL = "#"
+
 class Grid:
     def __init__(self) -> None:
         self.grid = [
@@ -18,18 +27,19 @@ class Grid:
     def draw(self, screen):
         for rowIndex, row in enumerate(self.grid):
             for colIndex, char in enumerate(row):
-                if char == "#":
-                    color = (255, 255, 255)
-                elif char == " ":
-                    color = (0, 0, 0)
-                elif char == "W":
-                    color = (255, 0, 0)
-                elif char == "B":
-                    color = (0, 0, 255)
+                colors = {
+                    BOX: (255, 0, 0),
+                    BOX_ON_STORAGE: (0, 255, 0),
+                    FLOOR: (0, 0, 0),
+                    PLAYER: (255, 0, 255),
+                    PLAYER_ON_STORAGE: (255, 0, 255),
+                    STORAGE: (0, 0, 255),
+                    WALL: (255, 255, 255)
+                }
 
                 pygame.draw.rect(
                     screen,
-                    color,
+                    colors[char],
                     (colIndex * 10, rowIndex * 10, 10, 10)
                 )
 
@@ -39,19 +49,19 @@ class Grid:
 
         if self.grid[new_y][new_x] == " ":
             self.grid[self.worker.y][self.worker.x] = " "
-            self.grid[new_y][new_x] = "W"
+            self.grid[new_y][new_x] = "@"
 
             self.worker.x = new_x
             self.worker.y = new_y
 
-        elif self.grid[new_y][new_x] == "B":
+        elif self.grid[new_y][new_x] == "$":
             box_x = new_x + x
             box_y = new_y + y
 
             if self.grid[box_y][box_x] == " ":
                 self.grid[self.worker.y][self.worker.x] = " "
-                self.grid[new_y][new_x] = "W"
-                self.grid[box_y][box_x] = "B"
+                self.grid[new_y][new_x] = "@"
+                self.grid[box_y][box_x] = "$"
 
                 self.worker.x = new_x
                 self.worker.y = new_y
