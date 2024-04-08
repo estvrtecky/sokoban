e6e792933f25b060b509635df9f8bd0364509d40
StratenyNinja <erik.stvrtecky2000@gmail.com>
2024-04-08 11:51:07 +0200

    Add game functionality and worker model

--- src/game.py
+++ src/game.py
@@ -1,5 +1,7 @@
 import pygame
 
+from .models import Worker
+
 
 class Game:
     def __init__(self):
@@ -7,5 +9,59 @@ class Game:
         pygame.init()
         pygame.font.init()
 
+        self.running = True
+        self.worker = Worker()
+        self.grid = [
+            ["#"] * 5,
+            ["#", " ", " ", " ", "#"],
+            ["#", " ", "W", " ", "#"],
+            ["#", " ", " ", " ", "#"],
+            ["#"] * 5
+        ]
+        self.draw()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.move_worker(0, -1)
+                    self.draw()
+                if event.key == pygame.K_DOWN:
+                    self.move_worker(0, 1)
+                    self.draw()
+                if event.key == pygame.K_LEFT:
+                    self.move_worker(-1, 0)
+                    self.draw()
+                if event.key == pygame.K_RIGHT:
+                    self.move_worker(1, 0)
+                    self.draw()
+
+    def move_worker(self, x, y):
+        new_x = self.worker.x + x
+        new_y = self.worker.y + y
+
+        if self.grid[new_y][new_x] == " ":
+            self.grid[self.worker.y][self.worker.x] = " "
+            self.grid[new_y][new_x] = "W"
+
+        self.worker.x = new_x
+        self.worker.y = new_y
+
+    def draw(self):
+        for row in self.grid:
+            print("".join(row))
+
     def run(self):
-        print("Game is running")
+        screen = pygame.display.set_mode((800, 600))
+        clock = pygame.time.Clock()
+        pygame.display.set_caption("Sokoban")
+
+        while self.running:
+            self.handle_events()
+
+            pygame.display.update()
+
+        pygame.quit()
---
+++ src/models/worker.py
@@ -0,0 +1,4 @@
+class Worker():
+    def __init__(self) -> None:
+        self.x = 2
+        self.y = 2
