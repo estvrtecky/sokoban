2c6dc8d770ba40c9ef687ba4498c5b7780d2a907
StratenyNinja <erik.stvrtecky2000@gmail.com>
2024-05-02 14:02:51 +0200

    Add sprites

--- src/graphics.py
+++ src/graphics.py
@@ -1,12 +1,26 @@
 import pygame
 
+from .config import Config
 from .models import BOX, BOX_ON_STORAGE, FLOOR, PLAYER, PLAYER_ON_STORAGE, STORAGE, WALL
 from .models import Level
 
 
 class Graphics:
     def __init__(self) -> None:
-        pass
+        self.config = Config("config.ini")
+        self.characters = [BOX, BOX_ON_STORAGE, FLOOR, PLAYER, PLAYER_ON_STORAGE, STORAGE, WALL]
+        self.sprites_path = self.config.get("paths", "sprites_path")
+        self.sprites_filenames = self.config.items("sprites")
+        self.sprites = self.load_sprites()
+
+    def load_sprites(self) -> dict[str, pygame.Surface]:
+        """Load the sprites from the sprites folder."""
+        sprites = {}
+        for charIndex, char in enumerate(self.characters):
+            _, filename = self.sprites_filenames[charIndex]
+            sprites[char] = pygame.image.load(f"{self.sprites_path}/{filename}")
+
+        return sprites
 
     def draw(self, screen: pygame.Surface, level: Level) -> None:
         """Draw the level on the screen."""
@@ -14,16 +28,7 @@ class Graphics:
         level_width, level_height = level.size()
         cell_size = min(screen_width // level_width, screen_height // level_height)
 
-        colors = {
-            BOX: (255, 0, 0),
-            BOX_ON_STORAGE: (0, 255, 0),
-            FLOOR: (0, 0, 0),
-            PLAYER: (255, 0, 255),
-            PLAYER_ON_STORAGE: (255, 0, 255),
-            STORAGE: (0, 0, 255),
-            WALL: (255, 255, 255)
-        }
-
         for y, row in enumerate(level.grid):
             for x, char in enumerate(row):
-                pygame.draw.rect(screen, colors[char], (x * cell_size, y * cell_size, cell_size, cell_size))
+                sprite = pygame.transform.scale(self.sprites[char], (cell_size, cell_size))
+                screen.blit(sprite, (x * cell_size, y * cell_size))
